68e68d392b812cce66b176dbad314f42ca484c3e
TobyYang1997 <56285707+TobyYang1997@users.noreply.github.com>
2020-11-21 22:35:07 +0100

    Update create_api.R

--- src/create_api.R
+++ src/create_api.R
@@ -1,19 +1,14 @@
-library(jsonlite)
-library(caret)
-library(dplyr)
-library(plumber)
-library(readr)
+#
 
 
-
-
-super_model <- read_rds("src/gbtree.rds")
 #* Sort df
 #* @param df
 #* @serializer json
 #* @post /credit_predict
 
 function(new_data) {
+  
+  super_model <- read_rds("src/gbtree.rds")
   
   predict(super_model, new_data, type = "prob")
 
