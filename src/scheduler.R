9557515cece70cf9858a2e8aa1191cb7c60aee8e
TobyYang1997 <56285707+TobyYang1997@users.noreply.github.com>
2020-11-23 00:31:16 +0100

    Update scheduler.R

--- src/scheduler.R
+++ src/scheduler.R
@@ -4,5 +4,5 @@ cmd <- cron_rscript("src/process_new_data.R")
 cron_add(command = cmd, frequency = "minutely", id="update_db", description = "update_db")
 #cron_add(command = cmd, frequency = "daily", id="test_1", description = "Just a testing Script")
 
-cron_ls()
-cron_clear()
+#cron_ls()
+#cron_clear()
